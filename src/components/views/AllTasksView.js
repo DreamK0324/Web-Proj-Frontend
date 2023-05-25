864ca395bf3115d910ce70b5e137392f2e22c851
Dream K <dreamk@ZoneBook.local>
2023-05-24 20:13:15 -0400

    random

--- src/components/views/AllTasksView.js
+++ src/components/views/AllTasksView.js
@@ -19,6 +19,7 @@ const AllTasksView = (props) => {
   
   return (
     <div>
+      <h1>All Tasks</h1>
       {tasks.map((task) => {
         let description = task.description;
         let priority_level = task.priority_level;
@@ -42,7 +43,9 @@ const AllTasksView = (props) => {
         <button>Add New Task</button>
       </Link>
       <br></br>
-      <Link to={`/`}>Home Page</Link>
+      <Link to={`/`}>
+        <button>Home Page</button>
+      </Link>
       
     </div>
   );
@@ -51,55 +54,3 @@ const AllTasksView = (props) => {
 
 export default AllTasksView;
 
-// import { Link } from "react-router-dom";
-
-// const AllTasksView = (props) => {
-//   let {tasks, deleteTask} = props;
-//   //tasks = [{id: 300, title: "hello"}]
-//   if (!tasks.length) {
-//     return (
-//     <div>
-//       <p>There are no tasks.</p>
-//       <Link to={`/newtask`}>
-//         <button>Add New Task</button>
-//       </Link>
-//       <br/>
-//       <br/>
-//       <Link to="/">Home Page</Link>
-//     </div>
-//     );
-//   }
-  
-//   return (
-//     <div>
-//       {tasks.map((task) => {
-//         let description = task.description;
-//         let priority_level = task.priority_level;
-//         let completion_status = task.completion_status;
-//         return (
-//           <div key={task.id}>
-//           <Link to={`/task/${task.id}`}>
-//             <h1>{description}</h1>
-//           </Link>
-//           <p>Priority: {priority_level}</p>
-//           <p>Status: {completion_status ? "Complete" : "Incomplete"}</p>
-
-
-
-//           <button onClick={() => deleteTask(task.id)}>Delete</button>
-//           </div>
-//         );
-//       }
-//       )}
-//       <Link to={`/newtask`}>
-//         <button>Add New Task</button>
-//       </Link>
-//       <br></br>
-//       <Link to={`/`}>Home Page</Link>
-      
-//     </div>
-//   );
-// };
-
-
-// export default AllTasksView;
